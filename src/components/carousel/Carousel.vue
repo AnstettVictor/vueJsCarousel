4945671e3d60aee0b884d080b4faa767708654b6
AnstettVictor <anstett.victor67@gmail.com>
2020-12-01 10:17:55 +0100

    mise en place button next prev avec controle index length

--- src/components/carousel/Carousel.vue
+++ src/components/carousel/Carousel.vue
@@ -1,11 +1,45 @@
 <template>
-    <div>
+    <div class="carousel"> 
         <slot></slot>
+        <button class="carousel__nav carousel__next" @click="next">Suivante</button>
+        <button class="carousel__nav carousel__prev" @click="prev">Précédente</button>
     </div>
 </template>
 
 <script>
 export default {
     
+    data() {
+       
+        return {
+            index: 0,
+            slides: []
+        }
+    },
+    computed: {
+        slidesCount() {
+            return this.slides.length
+        }
+    },
+    mounted() {
+        this.slides = this.$children
+        this.slides.forEach((slide, i) => {
+            slide.index = i
+        })
+    },
+    methods: {
+        next () {
+            this.index++
+            if (this.index >= this.slidesCount) {
+                this.index = 0
+            }
+        },
+        prev () {
+            this.index--
+            if (this.index < 0) {
+                this.index = this.slidesCount - 1
+            }
+        }
+    },
 }
 </script>
