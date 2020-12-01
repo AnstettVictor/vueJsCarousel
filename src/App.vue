9ba9e8e387ee1f0681994a181ba5936ff401bacd
AnstettVictor <anstett.victor67@gmail.com>
2020-12-01 11:21:20 +0100

    ajout slides dynamic

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
   <div>
     <carousel>
-
-      <carousel-slide>
+      <carousel-slide v-for="n in slides">
         <div style="position:absolute; left:0; right: 0; text-align:center; top: 50%; color: #FFF; font-size:16px;">  salut les gens </div>
-        <img src="https://picsum.photos/id/1/630/300" width="100%">
-      </carousel-slide>
-
-      <carousel-slide>
-      <div style="position:absolute; left:0; right: 0; text-align:center; top: 50%; color: #FFF; font-size:16px;"> au revoir les gens </div>
-        <img src="https://picsum.photos/id/2/630/300" width="100%">
+        <img :src="'https://picsum.photos/id/' + n + '/630/300'" width="100%">
       </carousel-slide>
     </carousel>
 
